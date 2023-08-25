b686bd1642daedf6ecedbbe12139b319617143c3
Lingua <None>
2023-08-25 12:28:06 +0200

    Lingua export Fri Aug 25 10:28:06 2023

--- getx_flutter/de_translation.dart
+++ getx_flutter/de_translation.dart
@@ -1,1 +1,5 @@
-final Map<String, String> de = {                    };
+final Map<String, String> de = {
+  "notification_body_9": "Lassen Sie uns gemeinsam die Entwicklung Ihrer Vitalit\u00e4t \u00fcberpr\u00fcfen.",
+  "welcome_back": "Willkommen zur\u00fcck!",
+  "vitality_program": "Je mange des"
+};
